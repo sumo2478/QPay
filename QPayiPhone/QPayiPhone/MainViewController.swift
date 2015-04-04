b832686f84f0d8d2f5b409fc13bd50a377194666
Gavin Chu <gavincca@gmail.com>
2015-04-04 15:39:32 -0700

    [IOS] added venmo authentication

--- QPayiPhone/QPayiPhone/MainViewController.swift
+++ QPayiPhone/QPayiPhone/MainViewController.swift
@@ -6,29 +6,55 @@
 //  Copyright (c) 2015 Gavin Chu. All rights reserved.
 //
 
+import Foundation
 import UIKit
 
 class MainViewController: UIViewController {
+    
+    @IBOutlet var button: UIButton!
+
+    var isLoggedIn = false;
 
     override func viewDidLoad() {
-        super.viewDidLoad()
+        super.viewDidLoad();
         
-        let bar:UINavigationBar! =  self.navigationController?.navigationBar
+        let bar:UINavigationBar! =  self.navigationController?.navigationBar;
         
-        bar.setBackgroundImage(UIImage(), forBarMetrics: UIBarMetrics.Default)
-        bar.shadowImage = UIImage()
-        bar.backgroundColor = UIColor.clearColor()
+        bar.setBackgroundImage(UIImage(), forBarMetrics: UIBarMetrics.Default);
+        bar.shadowImage = UIImage();
+        bar.backgroundColor = UIColor.clearColor();
+        
+        self.button.layer.borderColor = UIColor.darkGrayColor().CGColor;
+        self.button.layer.borderWidth = 1.0;
+        self.button.layer.cornerRadius = 4.0;
+        
+        if (Venmo.sharedInstance().isSessionValid()) {
+            self.isLoggedIn = true;
+        } else {
+            self.isLoggedIn = false;
+        }
     }
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
     }
     
-    @IBOutlet var buttonPressed: UIButton!
-    
     @IBAction func buttonPressed(sender: AnyObject) {
-        performSegueWithIdentifier("segueToScanner", sender: nil);
+        if (self.isLoggedIn) {
+            performSegueWithIdentifier("segueToScanner", sender: nil);
+        } else {
+
+            Venmo.sharedInstance().requestPermissions(["make_payments", "access_profile"], withCompletionHandler: { (var success, var error) -> Void in
+                if (success) {
+                    self.isLoggedIn = true;
+                    self.button.setTitle("Scan QR Code", forState: UIControlState.Normal);
+                } else {
+                    var alert = UIAlertController(title: "Authorization failed", message: error.localizedDescription, preferredStyle: UIAlertControllerStyle.Alert);
+                    alert.addAction(UIAlertAction(title: "OK", style: UIAlertActionStyle.Default, handler: nil));
+                    println(error);
+                }
+            });
+        }
     }
     
 }
