c5c2e9f285c3a56691ac2bf5e08e5329e49329bc
Gavin Chu <gavincca@gmail.com>
2015-04-04 12:34:07 -0700

    [IOS] added MainViewController.swift

---
+++ QPayiPhone/QPayiPhone/MainViewController.swift
@@ -0,0 +1,23 @@
+//
+//  MainViewController.swift
+//  QPayiPhone
+//
+//  Created by Gavin Chu on 4/4/15.
+//  Copyright (c) 2015 Gavin Chu. All rights reserved.
+//
+
+import UIKit
+
+class MainViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+}
