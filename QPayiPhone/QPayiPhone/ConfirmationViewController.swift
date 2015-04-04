b7a71228faf5bbe94e613f1b44dc2a0a89f6cb68
Collin Yen <collinyen7@gmail.com>
2015-04-04 16:06:56 -0700

    [ios] Implemented recording payments to parse

--- QPayiPhone/QPayiPhone/ConfirmationViewController.swift
+++ QPayiPhone/QPayiPhone/ConfirmationViewController.swift
@@ -9,6 +9,7 @@
 import Foundation
 
 class ConfirmationViewController: UIViewController {
+    var itemId:String = "";
     var itemTitle:String = "";
     var itemDescription:String = "";
     var itemAmount:UInt = 0;
@@ -22,6 +23,21 @@ class ConfirmationViewController: UIViewController {
     }
     
     @IBAction func pay(sender: AnyObject) {
+        recordPayment();
+    }
+    
+    func recordPayment() -> Void {
+        var completionHandler:(Bool!, NSError!) -> Void = {
+            (success:Bool!, error:NSError!) -> Void in
+            if success == true {
+                self.navigationController?.popToRootViewControllerAnimated(true);
+            }
+            else {
+                print("There was an error saving your payment");
+            }
+        }
         
+        let PaymentObject = PaymentModel();
+        PaymentObject.recordPaymentInParse(self.itemId, username: self.itemUserName, completionHandler: completionHandler);
     }
 }
