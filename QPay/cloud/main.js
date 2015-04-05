a761a007bb38f226df81c89c5acbe601c0fd3ba3
Collin Yen <collinyen7@gmail.com>
2015-04-04 17:03:40 -0700

    [PARSE] Added code for email and retrieving payment records

--- QPay/cloud/main.js
+++ QPay/cloud/main.js
@@ -19,7 +19,9 @@ Parse.Cloud.define("create", function(request, response) {
 	newItem.set("password", password);
 
 	newItem.save(null, {
-		success: function(item) {
+		success: function(item) {	
+			var itemLink = "http://www.google.com?code=" + item.id; 
+			//sendEmail(email, itemLink);
 			response.success(item.id);
 		},
 		error: function(item, error) {
@@ -31,22 +33,53 @@ Parse.Cloud.define("create", function(request, response) {
 /* Display Item Function */
 Parse.Cloud.define("retrieve", function(request, response) {
 	var itemId = request.params.id;
+	var promises = [];
 
 	var Item = Parse.Object.extend("items");
-	var query = new Parse.Query(Item);
-	query.get(itemId, {
-		success: function(item) {
-			// The object was retrieved successfully
-			var responseObject = {item: item};
-			/*var urlBase = "https://chart.googleapis.com/chart?";
-			var qrCodeUrl = encodeURIComponent("cht=qr&chs=400x400&chl=" + itemId);
-			responseObject['qr'] = urlBase + qrCodeUrl;*/
-			response.success(responseObject);
+	var queryItem = new Parse.Query(Item);
+	promises.push(queryItem.get(itemId, {
+		success: function (item) {
+			return item;
 		},
 		error: function(object, error) {
-			// The object failed to retrieve
-			response.error("Unable to retrieve item with this id");
+			return null;
 		}
-	});
+	}));
+
+	var Payments = Parse.Object.extend("payments");
+	var queryPayments = new Parse.Query(Payments);
+	queryPayments.equalTo("itemId", itemId);
+	promises.push(queryPayments.find({
+		success: function (payments) {
+			return payments;
+		},
+		error: function(object, error) {
+			return null;
+		}
+	}));
+
+	Parse.Promise.when(promises).then(
+		function(itemData, payments) {
+			if (!itemData || !payments) {
+				response.error("Unable to find item with this id");
+			}
+
+			var responseObject = {item: itemData, payments: payments};
+			response.success(responseObject);
+		}
+	)
 });
+
+// Helper function
+function sendEmail(emailToSendTo, link) {
+	var sendgrid = require("sendgrid");
+
+	sendgrid.initialize("sumo2478", "nothing2478");
+	sendgrid.sendEmail({
+		to:[emailToSendTo],
+		from: "collinyen7@gmail.com",
+		subject: "Your new QPay!",
+		text: "Use the following link to access your QPay Item Page: " + link,
+	});
+}
 
